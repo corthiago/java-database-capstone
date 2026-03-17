63f780ec2b2c8c2aa04ada47742bc06b8edb8c2e
Thiago Correia <cor.thiago@gmail.com>
2026-03-17 14:08:55 -0300

    setup Thymeleaf and create controller

--- app/src/main/java/com/project/back_end/mvc/DashboardController.java
+++ app/src/main/java/com/project/back_end/mvc/DashboardController.java
@@ -1,6 +1,9 @@
-package com.project.back_end.mvc;
-
-public class DashboardController {
+//package com.project.back_end.mvc;
+//
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//public class DashboardController {
 
 // 1. Set Up the MVC Controller Class:
 //    - Annotate the class with `@Controller` to indicate that it serves as an MVC controller returning view names (not JSON).
@@ -27,4 +30,46 @@ public class DashboardController {
 //    - If the token is invalid, redirects to the root URL.
 
 
+//}
+
+
+package com.project.back_end.mvc;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.project.back_end.services.Service;
+
+@Controller
+public class DashboardController {
+
+
+    @Autowired
+    Service service;
+
+    @GetMapping("/adminDashboard/{token}")
+    public String adminDashboard(@PathVariable String token) {
+        Map<String, String> map = service.validateToken(token, "admin").getBody();
+        System.out.println("map" + map);
+        if (map.isEmpty()) {
+            return "admin/adminDashboard";
+        }
+        return "redirect:http://localhost:8080";
+
+    }
+
+    @GetMapping("/doctorDashboard/{token}")
+    public String doctorDashboard(@PathVariable String token) {
+        Map<String, String> map = service.validateToken(token, "doctor").getBody();
+        System.out.println("map" + map);
+        if (map.isEmpty()) {
+            return "doctor/doctorDashboard";
+        }
+
+        return "redirect:http://localhost:8080";
+
+    }
 }
